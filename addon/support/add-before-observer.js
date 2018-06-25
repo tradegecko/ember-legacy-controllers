f38e396d9320f8ea72e9fc23ca376863cd6cf844
billybonks <sebastienstettler@gmail.com>
2018-06-25 19:20:55 +0800

    remember to export bro

--- addon/support/add-before-observer.js
+++ addon/support/add-before-observer.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import beforeEvent from './before-event';
 
-function _addBeforeObserver(obj, path, target, method) {
+export default function _addBeforeObserver(obj, path, target, method) {
   Ember.addListener(obj, beforeEvent(path), target, method);
   Ember.watch(obj, path);
 
